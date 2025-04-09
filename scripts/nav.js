b1d7d21bd13928fb931868ee52574b8acedde342
Josh <jvader.megamind@gmail.com>
2025-04-09 14:16:30 -0500

    React + TS Vite Template

--- scripts/nav.js
+++
@@ -1,8 +0,0 @@
-function navFunction() {
-    var topnav = document.getElementById("nav");
-    if (topnav.className === "nav") {
-      topnav.className += " responsive";
-    } else {
-      topnav.className = "nav";
-    }
-  }

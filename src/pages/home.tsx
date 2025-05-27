5511c67cc2c3e17230c87c60792f6272d6764b9d
Josh <jvader.megamind@gmail.com>
2025-05-27 10:37:04 -0500

    Fetch API on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
+
 export default function HomePage() {
+  const [status, setStatus] = useState('Fetching...');
+
+  useEffect(() => {
+    fetch(`https://jvader-api.onrender.com/`)
+    .then(response => {
+      if (!response.ok) {
+        console.log(`HTTP error! status: ${response.status}`);
+        setStatus('Offline');
+      }
+      else {
+        setStatus('Online');
+      }
+    })
+    .catch(error => {
+      setStatus('Offline');
+    })
+  }, []);
+
   return (
     <div>
       <h1 className="text-5xl text-center text-white mb-10">
         Jvader's Website
       </h1>
       <div className="bg-slate-800 rounded-xl shadow-xl px-4 py-3 m-2">
-        <p className="text-lg text-white">What</p>
-        <p className="text-base text-gray-400">how</p>
+        <p className="text-lg text-white">API Status</p>
+        <p className="text-base text-gray-400">{status}</p>
       </div>
     </div>
   );

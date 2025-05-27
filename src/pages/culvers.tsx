d72d939957caccfbf9324283edace13b34928c8d
Josh <jvader.megamind@gmail.com>
2025-05-27 16:02:24 -0500

    Better Culvers UI for today

--- src/pages/culvers.tsx
+++ src/pages/culvers.tsx
@@ -6,6 +6,8 @@ interface Flavors {
 
 interface Location {
   slug: string;
+  description: string;
+  flavor: string;
   flavors: Flavors;
 }
 
@@ -14,6 +16,7 @@ type LocationList = Location[];
 export default function CulversPage() {
   const [inputValue, setInputValue] = useState('');
   const [zip, setZip] = useState('');
+  const [isToday, setIsToday] = useState(true);
   const [data, setData] = useState<LocationList>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
@@ -22,7 +25,7 @@ export default function CulversPage() {
     setLoading(true);
     setData([]);
     setError('');
-    fetch(`https://jvader-api.onrender.com/culvers/search?name=${zip}`)
+    fetch(`https://jvader-api.onrender.com/culvers/search?name=${zip}&today=${isToday}`)
     .then(response => {
       if (!response.ok) console.log(`HTTP error! status: ${response.status}`);
       return response.json();
@@ -73,17 +76,40 @@ export default function CulversPage() {
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          className="w-full text-gray-300 px-4 py-2 border border-slate-300 rounded-xl shadow-xl focus:outline-none focus:ring-2 focus:ring-slate-300"
+          className="w-full text-gray-300 px-4 py-2 border border-slate-300 rounded-xl shadow-xl focus:outline-none focus:ring-2 focus:ring-slate-300 mb-4"
           placeholder="Type something..."
         />
+        <input
+          type="checkbox"
+          id="todayCheckbox"
+          onChange={(e) => setIsToday(e.target.checked)}
+          className="w-3 h-3 rounded-lg"
+          defaultChecked={true}
+        />
+        <label htmlFor="todayCheckbox" className="pl-2 text-gray-400">
+          Flavors just for today?
+        </label>
         <button type="submit" className="mt-4 w-full bg-slate-950 text-white py-2 px-4 rounded-xl shadow-xl hover:bg-slate-700 transition-colors">
           Search
         </button>
       </form>
       <div className="flex justify-center">
         {loading && <p className="text-white">Loading...</p>}
+
         {error && <p className="text-white">Error: {error}</p>}
-        {data && data.length > 0 && (
+
+        {isToday && data && data.length > 0 && (
+          <div className="grid grid-cols-1 mb-10">
+            {data.map((location) => (
+              <div className="bg-slate-800 rounded-xl shadow-xl px-4 py-3 m-2">
+                <p className="text-lg text-white">{location.description}</p>
+                <p className="text-base text-gray-400">{location.flavor}</p>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {!isToday && data && data.length > 0 && (
           <table className="table-auto border border-slate-700 text-white shadow-xl">
             <thead>
               <tr>
